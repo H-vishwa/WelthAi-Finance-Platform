8838a01ea1b7271f1b607dc13bef66d49cfa471e
H-vishwa <130923885+H-vishwa@users.noreply.github.com>
2025-11-21 15:07:58 +0530

    Updated files

---
+++ app/api/arcjet/route.js
@@ -0,0 +1,16 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+export default createMiddleware(aj);
